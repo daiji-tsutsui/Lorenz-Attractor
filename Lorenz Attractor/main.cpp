bfd68091755caa14b84bc2173da7864dbcd4fb15
Daiji Tsutsui <41978972+daiji-tsutsui@users.noreply.github.com>
2018-08-09 16:36:27 +0900

    2018.8.9 Lorenz Attractor 上に墨流しを実装（メモリリークの危険性あり）

--- Lorenz Attractor/main.cpp
+++ Lorenz Attractor/main.cpp
@@ -14,7 +14,7 @@
 #define R	28.0
 #define B	8.0/3.0
 #define EPS	0.001
-#define T	1000
+#define T	60
 using namespace std;
 
 random_device rnd;     // 非決定的な乱数生成器
@@ -22,6 +22,7 @@ mt19937 mt(rnd());  // メルセンヌ・ツイスタの32ビット版、引数�
 uniform_real_distribution<> unif(0.0, 1.0);   // [0.0,1.0]上一様に分布させる
 normal_distribution<> gauss(0.0, 1.0);   // 平均0.0、標準偏差1.0で分布させる
 
+int lorenzFlg = 2;	// 0: 回転, 1: グリグリ, 2: 墨流し
 int winh = 800;
 int winw = 1000;
 double xMin = -20.0;
@@ -31,7 +32,8 @@ double yMax = 20.0;
 double zMin = 0.0;
 double zMax = 40.0;
 int itrNum = T / EPS;
-double *x, *y, *z;
+int seqNum = 8000;
+double **x, **y, **z;
 double inix = 1.0;
 double iniy = 1.0;
 double iniz = 1.0;
@@ -62,21 +64,32 @@ void setup(void) {
 }
 void resize(int width, int height) {
 	glViewport(0, 0, width, height);
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	gluPerspective(45.0,
-				   (double)width/height,
-				   1.0,
-				   100.0);
-	gluLookAt(0.0, 0.0, 2.5,       //Position of Camera
-			  0.0, 0.0, 0.0,        //Position of Object
-			  0.0, 1.0, 0.0);       //Upward direction of Camera
-	glMatrixMode(GL_MODELVIEW);
+	if(lorenzFlg == 0 || lorenzFlg == 1){
+		glMatrixMode(GL_PROJECTION);
+		glLoadIdentity();
+		gluPerspective(45.0,
+					   (double)width/height,
+					   1.0,
+					   100.0);
+		gluLookAt(0.0, 0.0, 2.5,       //Position of Camera
+				  0.0, 0.0, 0.0,        //Position of Object
+				  0.0, 1.0, 0.0);       //Upward direction of Camera
+		glMatrixMode(GL_MODELVIEW);
+	}else if(lorenzFlg == 2){
+		glLoadIdentity();
+	}
 }
 void timer(int value) {
 	glutPostRedisplay();
-	glutTimerFunc(30 , timer , 0);
-	if(cnt < itrNum) cnt++;
+	if(lorenzFlg == 0){
+		glutTimerFunc(30 , timer , 0);
+	}else if(lorenzFlg == 2){
+		glutTimerFunc(10 , timer , 0);
+	}
+	if(cnt < itrNum-10){
+		cnt += 10;
+		if(cnt % 1000 == 0) cout << cnt << endl;
+	}
 }
 void mouse(int button, int state, int cx, int cy)
 {
@@ -91,10 +104,10 @@ void mouse(int button, int state, int cx, int cy)
 		}
 	}
 }
-void motion(int x, int y){
-	if(btnFlg){
-		t = t0 - 0.005 * (x - px);
-		s = max(min(s0 + 0.005 * (y - py), M_PI/2), -M_PI/2);
+void motion(int cx, int cy){
+	if(lorenzFlg == 1 && btnFlg){
+		t = t0 - 0.005 * (cx - px);
+		s = max(min(s0 + 0.005 * (cy - py), M_PI/2), -M_PI/2);
 		glutPostRedisplay();
 	}
 }
@@ -105,42 +118,66 @@ void display(void){
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	
 	glLoadIdentity();
-	gluLookAt(r*cos(s)*sin(t), r*sin(s), r*cos(s)*cos(t),
-			  0.0, 0.0, 0.0,
-			  0.0, 1.0, 0.0);
-//	glRotated(t, 0.0, 1.0, 0.0);
-//	if (t >= 360){ t = 0.0; }else{ t += 2.0; }
-
-	//x-axis
-	glColor3d(1.0, 0.0, 0.0);	//Black
-	glBegin(GL_LINES);
-		glVertex3d(-2.0, 0.0, 0.0);
-		glVertex3d(2.0, 0.0, 0.0);
-	glEnd();
-	//y-axis
-	glColor3d(0.0, 1.0, 0.0);	//Black
-	glBegin(GL_LINES);
-		glVertex3d(0.0, -2.0, 0.0);
-		glVertex3d(0.0, 2.0, 0.0);
-	glEnd();
-	//z-axis
-	glColor3d(.0, 0.0, 1.0);	//Black
-	glBegin(GL_LINES);
-		glVertex3d(0.0, 0.0, -2.0);
-		glVertex3d(0.0, 0.0, 2.0);
-	glEnd();
+	if(lorenzFlg == 0){
+		gluLookAt(0.0, 0.0, 2.5,
+				  0.0, 0.0, 0.0,
+				  0.0, 1.0, 0.0);
+		glRotated(t, 0.0, 1.0, 0.0);
+		if (t >= 360){ t = 0.0; }else{ t += 2.0; }
+	}else if(lorenzFlg == 1){
+		gluLookAt(r*cos(s)*sin(t), r*sin(s), r*cos(s)*cos(t),
+				  0.0, 0.0, 0.0,
+				  0.0, 1.0, 0.0);
+	}else if(lorenzFlg == 2){
+		glOrtho(-1.5, 1.5, -1.2, 1.8, -1.5, 1.5);
+		glRotated(90.0, 1.0, 0.0, 0.0);
+	}
 	
-	//trajectoryの描画
-//	for(int i = 1; i < cnt; i++){
-	for(int i = itrNum*9/10; i < itrNum; i++){
-		glColor3d(0.2, cos((double)i/(double)itrNum), sin((double)i/(double)itrNum));
+	if(lorenzFlg == 0 || lorenzFlg == 1){
+		//x-axis
+		glColor3d(1.0, 0.0, 0.0);	//Red
 		glBegin(GL_LINES);
-			glVertex3d(1.0-2.0*(x[i-1]-xMin)/(xMax-xMin),
-					   1.0-2.0*(y[i-1]-yMin)/(yMax-yMin),
-					   1.0-2.0*(z[i-1]-zMin)/(zMax-zMin));
-			glVertex3d(1.0-2.0*(x[i]-xMin)/(xMax-xMin),
-					   1.0-2.0*(y[i]-yMin)/(yMax-yMin),
-					   1.0-2.0*(z[i]-zMin)/(zMax-zMin));
+			glVertex3d(-2.0, 0.0, 0.0);
+			glVertex3d(2.0, 0.0, 0.0);
+		glEnd();
+		//y-axis
+		glColor3d(0.0, 1.0, 0.0);	//Green
+		glBegin(GL_LINES);
+			glVertex3d(0.0, -2.0, 0.0);
+			glVertex3d(0.0, 2.0, 0.0);
+		glEnd();
+		//z-axis
+		glColor3d(0.0, 0.0, 1.0);	//Blue
+		glBegin(GL_LINES);
+			glVertex3d(0.0, 0.0, -2.0);
+			glVertex3d(0.0, 0.0, 2.0);
+		glEnd();
+		
+		//trajectoryの描画
+		for(int k = 0; k < seqNum; k++){
+		//	for(int i = 0; i < cnt; i++){
+			for(int i = itrNum*9/10; i < itrNum; i++){
+				glColor3d((double)k/seqNum, cos((double)i/itrNum), sin((double)i/itrNum));
+				glBegin(GL_LINES);
+					glVertex3d(1.0-2.0*(x[k][i-1]-xMin)/(xMax-xMin),
+							   1.0-2.0*(y[k][i-1]-yMin)/(yMax-yMin),
+							   1.0-2.0*(z[k][i-1]-zMin)/(zMax-zMin));
+					glVertex3d(1.0-2.0*(x[k][i]-xMin)/(xMax-xMin),
+							   1.0-2.0*(y[k][i]-yMin)/(yMax-yMin),
+							   1.0-2.0*(z[k][i]-zMin)/(zMax-zMin));
+				glEnd();
+			}
+		}
+	}else if(lorenzFlg == 2){
+		//particlesの描画
+		glPointSize(3.0);
+		glColor3d(0.5, 0.1, 0.1);
+		glBegin(GL_POINTS);
+				for(int k = 0; k < seqNum; k++){
+					glVertex3d(1.0-2.0*(x[k][cnt]-xMin)/(xMax-xMin),
+							   1.0-2.0*(y[k][cnt]-yMin)/(yMax-yMin),
+							   1.0-2.0*(z[k][cnt]-zMin)/(zMax-zMin));
+				}
 		glEnd();
 	}
 	
@@ -151,17 +188,30 @@ void display(void){
 /*--Main func-------------------------------------------------------------------------*/
 int main(int argc, char * argv[]) {
 	/*--trajectoryの計算-------*/
-	x = (double *)calloc(itrNum, sizeof(double));
-	y = (double *)calloc(itrNum, sizeof(double));
-	z = (double *)calloc(itrNum, sizeof(double));
-	x[0] = inix; y[0] = iniy; z[0] = iniz;
+	if(lorenzFlg == 0 || lorenzFlg == 1) seqNum = 1;
+	cout << "itrNum: " << itrNum << endl
+		<< "seqNum: " << seqNum << endl;
 	
-	for(int i = 1; i < itrNum; i++){
-		x[i] = x[i-1] + EPS * dx(x[i-1],y[i-1],z[i-1]);
-		y[i] = y[i-1] + EPS * dy(x[i-1],y[i-1],z[i-1]);
-		z[i] = z[i-1] + EPS * dz(x[i-1],y[i-1],z[i-1]);
+	x = (double **)calloc(seqNum, sizeof(double *));
+	y = (double **)calloc(seqNum, sizeof(double *));
+	z = (double **)calloc(seqNum, sizeof(double *));
+	for(int k = 0; k < seqNum; k++){
+		x[k] = (double *)calloc(itrNum, sizeof(double));
+		y[k] = (double *)calloc(itrNum, sizeof(double));
+		z[k] = (double *)calloc(itrNum, sizeof(double));
+		x[k][0] = inix + unif(mt) * 0.001;
+		y[k][0] = iniy + unif(mt) * 0.001;
+		z[k][0] = iniz + unif(mt) * 0.001;
 	}
-	cout << itrNum << endl;
+	
+	for(int k = 0; k < seqNum; k++){
+		for(int i = 1; i < itrNum; i++){
+			x[k][i] = x[k][i-1] + EPS * dx(x[k][i-1],y[k][i-1],z[k][i-1]);
+			y[k][i] = y[k][i-1] + EPS * dy(x[k][i-1],y[k][i-1],z[k][i-1]);
+			z[k][i] = z[k][i-1] + EPS * dz(x[k][i-1],y[k][i-1],z[k][i-1]);
+		}
+		if(k % 100 == 0) cout << k << " / " << seqNum << endl;
+	}
 	
 //	getchar();
 	
@@ -175,11 +225,13 @@ int main(int argc, char * argv[]) {
 	glutMouseFunc(mouse);
 	glutMotionFunc(motion);
 //	glutIdleFunc(idle);
-//	glutTimerFunc(100 , timer , 0);
+	if(lorenzFlg == 0 || lorenzFlg == 2) glutTimerFunc(100 , timer , 0);
 	setup();
 	glutMainLoop();
 	
-	
+	for(int k = 0; k < seqNum; k++){
+		free(x[k]); free(y[k]); free(z[k]);
+	}
 	free(x); free(y); free(z);
 	return 0;
 }
